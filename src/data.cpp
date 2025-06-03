0ca55043d554383d6ea7db3ca83bdbfbc75b27de
Isaías Gouvêa Gonçalves <isaias.ggoncalves@gmail.com>
2025-06-03 19:14:01 -0300

    implementei uma função que lê todos os arquivos e armazena as palavras numa estrutura própria

--- src/data.cpp
+++ src/data.cpp
@@ -6,14 +6,18 @@
 
 namespace DATA {
    
-std::vector<std::string> readFromFile(std::string& fileName){
+
+std::vector<std::string> readFromFile(std::string& filePath){
+
+   // Essa função basicamente lê uma arquivo (filePath) e retorna um vetor com todas as suas palavras
+
 
    std::vector<std::string> words;
-   std::ifstream file(fileName);
+   std::ifstream file(filePath);
    std::string word;
 
    if (!file.is_open()){
-      std::cerr << "Erro ao abrir o arquivo"<< fileName << std::endl;
+      std::cerr << "Erro ao abrir o arquivo"<< filePath << std::endl;
       return words; 
    }
 
@@ -25,5 +29,26 @@ std::vector<std::string> readFromFile(std::string& fileName){
    return words;
 
 }
+
+std::vector<FileData>* readFiles(int amount){
+
+   // Essa função recebe um inteiro positivo que dita quantos arquivos serão lidos
+   // A função lê cada um dos arquivos X.txt e para cada arquivo cria um objeto FileData
+   // onde serão armazenados o ID do arquivo e um vetor com suas palavras
+   // Tudo isso ao final é armazenado no vetor files e retornado
+
+   std::vector<FileData>* files = new std::vector<FileData>;
+
+   for (int i = 0; i <= amount; ++i){
+      std::string filePath = "..\\data\\" + std::to_string(i) + ".txt";
+      std::vector<std::string> words = readFromFile(filePath);
+      FileData* file = new FileData;
+      file->fileID = i;
+      file->words = words;
+      files->push_back(*file);
+   }
+   return files;
+}
+
 
 }
--- src/data.h
+++ src/data.h
@@ -1,14 +1,18 @@
-#ifndef DATA
-#define DATA
+#ifndef DATA_H
+#define DATA_H
 
 #include <iostream>
 #include <string>
 #include <vector>
 
+struct FileData{
+   int fileID;
+   std::vector<std::string> words;
+};
+
 namespace DATA {
 std::vector<std::string> readFromFile(std::string& fileName);
-
-
+std::vector<FileData>* readFiles(int amount);
 
 }
 
